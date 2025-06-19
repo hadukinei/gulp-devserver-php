e889acc9e62ba8e2c5056db08c31d7cc80c633d0
hadukinei <quiltqueen73344614@yahoo.co.jp>
2025-06-19 22:28:07 +0900

    tmp

---
+++ test/old/src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>test</title>
+    <script src="index.js"></script>
+  </html>
+  <body>
+    <h1>test</h1>
+    <?php
+      echo '<h2>tetest</h2>';
+    ?>
+  </body>
+</html>
---
+++ test/old/src/index.ts
@@ -0,0 +1,5 @@
+(d => {
+  d.addEventListener('DOMContentLoaded', () => {
+    console.log('fired')
+  })
+})(document)
